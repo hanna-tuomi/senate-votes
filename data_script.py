5e77a239080257c39da24f807cc087b8f9f6b908
hanna-tuomi <hanna.tuomi@gmail.com>
2020-08-19 21:17:49 -0400

    Add dicts to txt files

--- data_script.py
+++ data_script.py
@@ -10,3 +10,16 @@ congress_dict = {}
 
 urls = get_vote_links()
 insert_dicts(urls, bill_dict, senator_dict, congress_dict)
+
+# write the dictionaries into txt files
+bill_txt = open("bill_dict.txt","w")
+bill_txt.write(str(bill_dict))
+bill_txt.close()
+
+senator_txt = open("senator_dict.txt","w")
+senator_txt.write(str(senator_dict))
+senator_txt.close()
+
+congress_txt = open("congress_dict.txt","w")
+congress_txt.write(str(congress_dict))
+congress_txt.close()
