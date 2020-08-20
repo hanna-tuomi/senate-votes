4881d1049033a5d7e7832313930aee67df44dd49
hanna-tuomi <hanna.tuomi@gmail.com>
2020-08-19 21:06:39 -0400

    Refined webscraper and url cleaning

--- webscraper.py
+++ webscraper.py
@@ -1,34 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_roll_call_list_links(url):
+def get_roll_call_list_links(url, url_segment):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     links = []
     for link in soup.find_all('a'):
         next_page = link.get('href')
-        if type(next_page) is str and 'roll_call_lists' in next_page:
+        if type(next_page) is str and url_segment in next_page:
             links.append(next_page)
 
-    # clean the links
-    clean_links = set()
+    # clean the links to include the https etc
+    clean_links = []
     for l in links:
         if 'senate.gov' not in l:
-            clean_links.add('https://www.senate.gov' + l)
+            clean_links.append('https://www.senate.gov' + l)
         else:
-            clean_links.add(l)
+            clean_links.append(l)
+
     return clean_links
 
 def get_vote_links():
     og_url = 'https://www.senate.gov/reference/Index/Votes.htm'
-    congresses = get_roll_call_list_links(og_url)
+    # get each document from a specific congress and session
+    congresses = get_roll_call_list_links(og_url, 'roll_call_lists/vote_menu')
+
+    # get each vote record
     documents = []
     for link in congresses:
-        documents.extend(get_roll_call_list_links(link))
+        documents.extend(get_roll_call_list_links(link, 'roll_call_lists/roll_call_vote_cfm'))
+
+    # get the xml
     votes = []
     for link in documents:
-        votes.extend(get_roll_call_list_links(link))
+        votes.extend(get_roll_call_list_links(link, 'xml'))
     return votes
-
-
-print(get_vote_links)
